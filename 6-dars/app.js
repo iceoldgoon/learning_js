806fbd4226f74d799a93013f60b35a21fe6d96f5
Iceoldgoon <islomjonqurbonboyev52@gmail.com>
2026-08-21 18:41:07 +0500

    app-js va index-html fayllari qoshildi, malumot faylida ozgaruvchilar haqida malumot kiritildi

---
+++ 6-dars/app.js
@@ -0,0 +1,14 @@
+let ism = 'islomjon'
+let yosh = 18
+
+    yosh = 20
+
+console.log(ism, yosh)
+
+const familiya = 'Qurbonboyev'
+
+console.log(familiya)
+
+var joy = 'Toshkent'
+joy = 'Samarqand'
+console.log(joy)
