30ba1461588c2a28bcbcea65e1daa7986feb3a59
Iceoldgoon <islomjonqurbonboyev52@gmail.com>
2026-08-23 18:33:24 +0500

    app.js va index.html fayllari qoshildi stringlar bilan ishlash boyicha misollar kiritildi

---
+++ 9-dars/app.js
@@ -0,0 +1,35 @@
+// string
+
+console.log("Hello, World!")
+
+let email = 'islomjon@gmail.com'
+
+console.log(email)
+
+//string concatenation
+let firstName = 'Islomjon'
+let lastName = 'Qurbonboyev'
+
+let fullName = firstName + ' ' + lastName
+
+console.log(fullName)
+
+//getting characters
+console.log(fullName[0])
+
+//string length => uzunlik
+
+console.log(fullName.length)
+
+//string methods
+
+console.log(fullName.toUpperCase())
+
+let result = fullName.toLowerCase()
+
+console.log(result)
+
+let index = fullName.indexOf('j')
+
+console.log(index)
+
