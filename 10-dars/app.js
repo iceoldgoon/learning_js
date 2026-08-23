d1743d8e9a2b411d8ab6a99e8a7a99328109d6e5
Iceoldgoon <islomjonqurbonboyev52@gmail.com>
2026-08-23 19:01:19 +0500

    app.js va index.html fayllari qoshildi stringlar bilan ishlash boyicha misollar kiritildi

---
+++ 10-dars/app.js
@@ -0,0 +1,27 @@
+let email = ' islomjon@gmail.com '
+
+// lastIndexOf() - oxirgi indeksni qaytaradi
+//
+// let lastIndexOf = email.lastIndexOf('m')
+// console.log(lastIndexOf)
+//
+
+// slice() - berilgan indeksdan boshlab stringni kesib oladi
+
+// let result = email.slice(0, 5)
+// console.log(result) // islom
+
+// let result = email.slice(1, 9)
+// console.log(result) // slomjon
+
+// let result = email.replace('islomjon', 'islom')
+// console.log(result)
+
+// let result = email.charAt()
+// console.log(result)
+
+// let result = email.trim()
+// console.log(result.length, email.length)
+
+// let result = email.split('')
+// console.log(result)
