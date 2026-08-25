f4b39aaf6bd52b2d816dca580609fe5967c93770
Iceoldgoon <islomjonqurbonboyev52@gmail.com>
2026-08-25 16:41:50 +0500

    app.js va index.html fayllari qoshildi, booleans va taqqoslash operatorlari bilan ishlash boyicha misollar kiritildi

---
+++ 15-dars/app.js
@@ -0,0 +1,24 @@
+// booleans & comparisons
+
+console.log(true, false, "true", "false")
+
+// methods can return booleans
+
+let email = 'user@example.com'
+
+let result = email.includes('3')
+
+// console.log(result)
+
+// comrions operators
+
+let age = 25
+
+// console.log(age == 25)
+// console.log(age == 30)
+// console.log(age != 25)
+// console.log(age > 20)
+// console.log(age < 20)
+// console.log(age <= 25)
+// console.log(age >= 25)
+
