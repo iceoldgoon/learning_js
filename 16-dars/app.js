f61f860646ca768ed9d1cb57dfbbaa342bc0a345
Iceoldgoon <islomjonqurbonboyev52@gmail.com>
2026-08-25 18:06:59 +0500

    app.js va index.html fayllari qoshildi, kuchsiz va kuchli tenglashtirish operatorlari bilan ishlash boyicha misollar kiritildi

---
+++ 16-dars/app.js
@@ -0,0 +1,16 @@
+let age = 25
+
+//kuchsiz tenglashtirish
+
+// console.log(age == 25)
+// console.log(age == '25')
+// console.log(age != 25)
+// console.log(age != 24)
+
+//kuchli tenglashtirish
+
+// console.log(age === 25)
+// console.log(age === '25')
+// console.log(age !== 25)
+// console.log(age !== '25')
+
