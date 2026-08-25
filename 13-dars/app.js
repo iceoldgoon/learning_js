3140c71f1fc482995e3c335e85039eea49b95401
Iceoldgoon <islomjonqurbonboyev52@gmail.com>
2026-08-25 11:49:13 +0500

    app.js va index.html fayllari qoshildi, mehmonlar massivini va asosiy JavaScript xususiyatlarini korsatish uchun misollar kiritildi

---
+++ 13-dars/app.js
@@ -0,0 +1,25 @@
+// const mehmon1 = 'Ali';
+// const mehmon2 = 'Vali';
+// const mehmon3 = 'Hasan';
+
+//array => massiv
+ const mehmonlar = ['Ali', 'Vali', 'Hasan', 12, 34];
+
+// mehmonlar[0] = 'Olim';
+// console.log(mehmonlar)
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//property => xususiyat
+// const result = mehmonlar.length;
+
+// console.log(result);
+
+// let result = mehmonlar.indexOf('Vali');
+// let result = mehmonlar.concat(numbers);
+
+// mehmonlar.push('islom')
+
+// mehmonlar.pop('vali')
+
+console.log(mehmonlar)
