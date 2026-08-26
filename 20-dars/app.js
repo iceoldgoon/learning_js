5559b065561ca2c5faf86ae2e9c791b55d685b2c
Iceoldgoon <islomjonqurbonboyev52@gmail.com>
2026-08-26 18:35:46 +0500

    app.js va index.html fayllari qoshildi, mehmonlar royxati va parol uzunligini tekshirish misollari kiritildi

---
+++ 20-dars/app.js
@@ -0,0 +1,17 @@
+// const age = 20
+
+// if (age > 15){
+    // console.log('siz katta baca eknsiz😯')
+// }
+
+const  mehmonlar = ['Ali', 'Vali', 'Gani', 'Tursinboy']
+
+if (mehmonlar.length == 3){
+    console.log('odam kop')
+}
+
+const parol = '123456789'
+
+if (parol.length >=8){
+    console.log('parol zor👌')
+}
