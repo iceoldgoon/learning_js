9f58384a67305a304fddf38ac8826094aa5cc7c5
Iceoldgoon <islomjonqurbonboyev52@gmail.com>
2026-08-26 12:00:30 +0500

    app.js va index.html fayllari qoshildi, natijalarni turli turlarga ozgartirish boyicha misollar kiritildi

---
+++ 17-dars/app.js
@@ -0,0 +1,13 @@
+let natija = '100'
+
+// natija = Number(natija)
+
+// console.log(natija + 1)
+
+// let result = String(15)
+
+// console.log(typeof result)
+
+// let result = Number('hello')
+
+// console.log(result)
