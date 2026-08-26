3633a176651b015a8dfef9e7499db8dfbe9b6666
Iceoldgoon <islomjonqurbonboyev52@gmail.com>
2026-08-26 16:15:40 +0500

    app.js va index.html fayllari qoshildi, massivlar bilan ishlash va do-while sikli misollari kiritildi

---
+++ 19-dars/app.js
@@ -0,0 +1,16 @@
+// const names =['islom', 'gani', 'vani']
+//
+// let i = 0
+//
+// while (i < names.length) {
+//     console.log(names[i].toLowerCase())
+//     i++
+// }
+
+//
+// let i = 5
+//
+// do{
+//     console.log(i)
+//     i++
+// } while (i < 5)
